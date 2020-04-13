1347ff6f69a13cc4b2a10de81a80c1d25bb2bdd4
Ashwani <ashwani.pandey@myqsoft.com>
2020-04-13 20:48:11 +0530

    done

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use('/api', routes);
 // });
 
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/frontent'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/frontent/index.html'));
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/mean_tutorial');
