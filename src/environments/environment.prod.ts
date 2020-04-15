ab48913acb3a22b4a4bffe1612a62d56648e8d63
Ashwani <ashwani.pandey@myqsoft.com>
2020-04-15 20:08:25 +0530

    done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   'Auth_Key': 'eyJhdXRoIjoiOWVkMjMwNzY1ZWU0MmExNGYxNzk0YjY5M2M1MmQ3NzUiLCJESVNUUklCVVRFRF9UT0tFTiI6IjU5ZWMzZDU4NDIxYjdmYzJiOWJkZjBkODU4MmQ2MWI0In0=',  
-  'API_URL': 'https://letsping.herokuapp.com/', 
+  'API_URL': 'https://letsping.herokuapp.com/api/', 
 };
